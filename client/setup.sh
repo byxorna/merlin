1fc385f6660e219f335a707c96dfcf371769f552
Gabe Conradi <gabe@tumblr.com>
2014-10-23 17:51:16 -0400

    haproxy stuff

--- client/setup.sh
+++ client/setup.sh
@@ -8,6 +8,7 @@ ETCD_PORT=4001
 echo "Using confdir $CONF_DIR"
 [[ ! -d $CONF_DIR/conf.d ]] && echo "No conf.d directory found at $CONF_DIR" && exit 1
 [[ ! -d $CONF_DIR/templates ]] && echo "No templates directory found at $CONF_DIR" && exit 1
-$CONFD -backend etcd -node $ETCD_SERVER:$ETCD_PORT -confdir $CONF_DIR -debug -watch
+#$CONFD -backend etcd -node $ETCD_SERVER:$ETCD_PORT -confdir $CONF_DIR -debug -watch
+$CONFD -backend etcd -node $ETCD_SERVER:$ETCD_PORT -confdir $CONF_DIR -debug -interval 3
 
 
